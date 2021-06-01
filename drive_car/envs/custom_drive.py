e71b2e95c054c4aea741c55c696380c6319d2449
RRRK-Malla <81765039+RRRK-Malla@users.noreply.github.com>
2021-06-02 05:58:31 +1000

    Update custom_drive.py

--- drive_car/envs/custom_drive.py
+++ drive_car/envs/custom_drive.py
@@ -8,22 +8,22 @@ class DriveEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-        self.drivezy = driveGame()
+        self.pygame = driveGame()
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(np.array([0, 0, 0, 0, 0]), np.array([10, 10, 10, 10, 10]), dtype=np.int)
 
     def reset(self):
-        del self.drivezy
-        self.drivezy = driveGame()
-        show = self.drivezy.view()
+        del self.pygame
+        self.pygame = driveGame()
+        show = self.pygame.view()
         return show
 
     def step(self, action):
-        self.drivezy.action(action)
-        show = self.drivezy.view()
-        reward = self.drivezy.eval()
+        self.pygame.action(action)
+        show = self.pygame.view()
+        reward = self.pygame.eval()
         comp = self.pygame.finish()
         return show, reward, comp, {}
 
     def render(self, mode="human", close=False):
-        self.drivezy.view()
+        self.pygame.view()
